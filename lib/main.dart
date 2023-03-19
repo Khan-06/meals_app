dffde2d383499a12f33d3b45549f5bf6f3257275
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-03-19 22:46:11 +0500

    Changing HomeScreen and adding some styling!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './Category_Screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,27 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const CategoriesScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Meals App'),
-        ),
-        body: const Center(
-          child: Text('Navigation Time!'),
-        ));
   }
 }
