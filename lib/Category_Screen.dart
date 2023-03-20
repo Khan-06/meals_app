34c90161b1c1480dfd55f0ade7d53da2a4441bdd
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-03-21 00:14:37 +0500

    Added some page navigation!

--- lib/Category_Screen.dart
+++ lib/Category_Screen.dart
@@ -10,7 +10,7 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child:  Text('Meals App')),
+        title:  Center(child:  Text('Meals App', style: Theme.of(context).textTheme.titleLarge,)),
       ),
       body: GridView(
         padding: const EdgeInsets.all(15),
--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
-   CategoryItem(this.title, this.color);
+  CategoryItem(this.title, this.color);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.6), color],
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) {
+              return const CategoryMealsScreen();
+            },
+          ),
+        );
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(17),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.6), color],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+          borderRadius: BorderRadius.circular(17),
         ),
-        borderRadius: BorderRadius.circular(17),
+        child: Text(title, style: Theme.of(context).textTheme.titleLarge),
       ),
-      child: Text(title, style: Theme.of(context).textTheme.titleLarge),
     );
   }
 }
