8fe645755ea2550d8b1cf3443aa5e692187a0442
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-03-22 20:58:24 +0500

    Image Card Improvement - Added price and Affordability

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -15,6 +15,30 @@ class MealItem extends StatelessWidget {
   final Affordability affordability;
   final int duration;
 
+  String get complexityText {
+    switch (complexity){
+      case Complexity.Simple:
+        return 'Simple';
+        case Complexity.Challenging:
+        return 'Challenging';
+        case Complexity.Hard:
+        return 'Hard';
+        default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Pricey:
+        return 'Pricey';
+      case Affordability.Luxurious:
+        return 'Luxurious';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -25,10 +49,63 @@ class MealItem extends StatelessWidget {
         margin: const EdgeInsets.all(10),
         child: Column(
           children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(15), topLeft: Radius.circular(15)),
-              child: Image.network(imageUrl, height: 250, width: double.infinity, fit: BoxFit.cover),
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(15),
+                      topLeft: Radius.circular(15)),
+                  child: Image.network(imageUrl,
+                      height: 250, width: double.infinity, fit: BoxFit.cover),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                  children: [
+                    Icon(Icons.schedule),
+                    const SizedBox(width: 6),
+                    Text('$duration min')
+                  ],
+                ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      const SizedBox(width: 6),
+                      Text(complexityText)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      const SizedBox(width: 6),
+                      Text(affordabilityText)
+                    ],
+                  )],
+              ),
             )
           ],
         ),
