49dede23cf781d42b0ed91e560df08dbc1c3705c
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-03-25 00:02:52 +0500

    Adding onUnknownRoute in main.dart file!

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,9 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => const MealDetailScreen(),
       },
+      onUnknownRoute: (setting){
+        return MaterialPageRoute(builder: (ctx) => const CategoriesScreen());
+      },
     );
   }
 }
